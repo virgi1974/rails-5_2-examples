41e24754cb2f111459568a4270a3042b8f6373bd
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-01 18:41:20 +0200

    TEST the rake task triggers an active job

---
+++ spec/lib/tasks/newsletter_tasks_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "newsletters:from_the_bridge" do
+
+	before do
+		ActiveJob::Base.queue_adapter.enqueued_jobs.clear
+	end
+
+	it "schedules background job to perform soon" do
+		subject.execute
+		expect(ActiveJob::Base.queue_adapter.enqueued_jobs.size).to eq(1)
+	end
+
+end
