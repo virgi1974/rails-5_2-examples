31a21c2aa30d039ef04a916362da17192680decf
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-01 20:28:18 +0200

    Mailer FromTheBridgeMailer triggered by ActiveJob
    - rails g mailer FromTheBridgeMailer send_newsletter

---
+++ spec/mailers/previews/from_the_bridge_mailer_preview.rb
@@ -0,0 +1,9 @@
+# # Preview all emails at http://localhost:3000/rails/mailers/from_the_bridge_mailer
+# class FromTheBridgeMailerPreview < ActionMailer::Preview
+
+#   # Preview this email at http://localhost:3000/rails/mailers/from_the_bridge_mailer/send_newsletter
+#   def send_newsletter
+#     FromTheBridgeMailerMailer.send_newsletter
+#   end
+
+# end
