9c899c32b99f407a15705c31a84cac1a30f6179e
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-01 19:21:35 +0200

    Config for FactoryBot
    - disables fixtures
    - loads FactoryBot syntax methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,12 +30,12 @@ Rails.application.load_tasks
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -58,4 +58,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.infer_rake_task_specs_from_file_location!
+
+  config.include FactoryBot::Syntax::Methods
 end
