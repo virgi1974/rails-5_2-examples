0ce0387e4b39799747685a70d0d670ccc318d95b
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-01 18:33:48 +0200

    Configuration to test active jobs in test env
    - queue adapter needs to be set

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # config to test active jobs
+  config.active_job.queue_adapter = :test
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
