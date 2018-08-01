16af462ebfa7429fb9f415e0156ca7af40f378ee
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-01 20:31:30 +0200

    TEST SendFromTheBridgeNewsletterJob performs triggers email for hacker

--- spec/jobs/send_from_the_bridge_newsletter_job_spec.rb
+++ spec/jobs/send_from_the_bridge_newsletter_job_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SendFromTheBridgeNewsletterJob, type: :job do
-  # pending "add some examples to (or delete) #{__FILE__}"
+
+	before do
+		ActionMailer::Base.deliveries.clear
+	end
+
+	after do
+		ActionMailer::Base.deliveries.clear
+	end
+
+	context "with small number of users" do
+		it "sends newsletters to all hackers (aka users)" do
+			hackers = FactoryBot.create_list(:hacker, 5)
+			SendFromTheBridgeNewsletterJob.perform_now(hackers)
+			expect(ActionMailer::Base.deliveries.count).to eq(5)
+		end
+	end
+
 end
