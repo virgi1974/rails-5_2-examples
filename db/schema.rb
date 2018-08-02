90391dcc1dbec82892ffd4b15b66dbbb84ebf9b7
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-02 18:10:05 +0200

    Genre scafold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_153638) do
+ActiveRecord::Schema.define(version: 2018_08_02_160718) do
+
+  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
