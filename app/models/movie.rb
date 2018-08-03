64daf2154f20fc6f46e507a321c396f4f3c73a99
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-03 19:16:18 +0200

    Elasticsearch indexes added to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,22 @@ class Movie < ApplicationRecord
 
 	has_and_belongs_to_many :genres
 
+	mapping do 
+	  indexes :id
+	  indexes :name
+	  indexes :description
+
+	  indexes :genres do
+	  	indexes :id
+	  	indexes :name
+	  end
+	end
+
+	def as_indexed_json(options = {})
+	  self.as_json(only: [:name, :description],
+	  include: {
+	  	genres: { only: [:id, :name]}
+	  })
+	end
+
 end
