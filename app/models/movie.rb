ca347565c384d697001d073ccffa917a15932b72
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-03 20:54:38 +0200

    Custom search method definition

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,4 +22,22 @@ class Movie < ApplicationRecord
 	  })
 	end
 
+	class << self
+
+		def custom_search(query)
+		__elasticsearch__.search(query: multi_match_query(query))		
+		end
+
+		def multi_match_query(query)
+			{
+				multi_match: {
+					query: query,
+					type: "best_fields", #other possible values "most_fields" "phrase" "phrase_prefix" "cross_fields"
+					fields: ["name^9", "description^8", "genres.name^10"],
+					operator: "and"
+				}
+			}
+		end
+	end
+
 end
