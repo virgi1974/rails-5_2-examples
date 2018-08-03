a2351e0d8cc44201d31812964d4739b864d3b9fe
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-03 19:19:04 +0200

    Filters using the params q query input from user

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies
   # GET /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.search((params[:q].present? ? params[:q] : '*')).records
   end
 
   # GET /movies/1
