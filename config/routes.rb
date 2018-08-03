7a6f3670a5cf5a3f7603392b4764f36c405726b9
virgilio1974 <vitrgilio1974@gmail.com>
2018-08-03 21:37:32 +0200

    Custom route with cleaner url to redirect searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+	resources :movies, only: [:show, :index] do
+		get 'search/*query', to: 'movies#index', as: :search, on: :collection
+	end
+
   resources :genres
   resources :movies
 
